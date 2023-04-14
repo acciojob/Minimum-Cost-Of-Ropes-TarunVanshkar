a544089c354967f84bb05ead79960508439355f4
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-15 00:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  // get the input value and convert it to an array of integers
+    const input = document.getElementById("rope-lengths").value;
+    const ropeArray = input.split(",").map(Number);   // .map(Number)  convert each element of array into number
+    
+    // sort the rope lengths in increasing order
+    ropeArray.sort((a, b) => a - b);
+    
+    // initialize the total cost to 0
+    let totalCostOfRope = 0;
+    
+    // combine the smallest two ropes at each step until only one rope is left
+    while (ropeArray.length > 1) {
+      const rope1 = ropeArray.shift(); // remove the first array element and return it
+      const rope2 = ropeArray.shift();
+      const combinedRope = rope1 + rope2; // combine the two ropes
+      totalCostOfRope += combinedRope; // add the cost of combining the two ropes
+      ropeArray.push(combinedRope); // add the combined rope back to the array
+    //   console.log(ropeArray, combinedRope);
+      ropeArray.sort((a, b) => a - b); // sort the array again in non-decreasing order
+    //   console.log(ropeArray)
+    }
+    
+    // display the minimum cost in the result div
+    document.getElementById("result").innerHTML = totalCostOfRope;
 }  
